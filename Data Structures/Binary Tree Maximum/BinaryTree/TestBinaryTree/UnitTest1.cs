fae397a65597a4851530a372a75b194d614184f8
LaithAlamat <laithalamat4@gmail.com>
2022-04-27 18:13:31 +0300

    tests added

--- Data Structures/Binary Tree Maximum/BinaryTree/TestBinaryTree/UnitTest1.cs
+++ Data Structures/Binary Tree Maximum/BinaryTree/TestBinaryTree/UnitTest1.cs
@@ -104,5 +104,45 @@ namespace TestBinaryTree
             Assert.False(Tree.Contains(Tree.Root, 30));
 
         }
+
+        [Fact]
+        public void TestMax()
+        {
+            Tree tree = new Tree();
+            tree.Root = new Node(1);
+            tree.Root.Left = new Node(2);
+            tree.Root.Right = new Node(3);
+            tree.Root.Left.Left = new Node(4);
+            tree.Root.Left.Right = new Node(5);
+            tree.Root.Right.Left = new Node(6);
+
+            Assert.Equal(6, tree.FindMax(tree.Root));
+        }
+        [Fact]
+        public void TestSameValue()
+        {
+            Tree tree = new Tree();
+            tree.Root = new Node(1);
+            tree.Root.Left = new Node(6);
+            tree.Root.Right = new Node(3);
+            tree.Root.Left.Left = new Node(6);
+            tree.Root.Left.Right = new Node(5);
+            tree.Root.Right.Left = new Node(6);
+
+            Assert.Equal(6, tree.FindMax(tree.Root));
+        }
+        [Fact]
+        public void TestNegative()
+        {
+            Tree tree = new Tree();
+            tree.Root = new Node(-1);
+            tree.Root.Left = new Node(-2);
+            tree.Root.Right = new Node(-3);
+            tree.Root.Left.Left = new Node(-4);
+            tree.Root.Left.Right = new Node(-5);
+            tree.Root.Right.Left = new Node(-6);
+
+            Assert.Equal(-1, tree.FindMax(tree.Root));
+        }
     }
 }
