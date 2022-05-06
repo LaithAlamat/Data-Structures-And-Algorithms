1c33f18d24871bd266fae51902ba964fb13a2842
LaithAlamat <laithalamat4@gmail.com>
2022-05-06 21:58:00 +0300

    Challenge Done!

---
+++ Data Structures/FizzBuzzKTree/FizzBuzzKTree/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace FizzBuzzKTree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FizzBuzzNode first = new FizzBuzzNode(5);
+            FizzBuzzNode sec = new FizzBuzzNode(6);
+            FizzBuzzNode third = new FizzBuzzNode(9);
+            FizzBuzzNode fourth = new FizzBuzzNode(15);
+            FizzBuzzNode fifth = new FizzBuzzNode(4);
+            first.Children.Add(sec);
+            first.Children.Add(third);
+            first.Children.Add(fourth);
+            first.Children.Add(fifth);
+
+            KTree tree = new KTree(first);
+            tree.FizzBuzzTreeMethod(tree);
+        }
+    }
+}
