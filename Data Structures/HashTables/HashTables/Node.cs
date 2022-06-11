0b44c19e4883b2c5a546dc2ba42b8fb31839989e
LaithAlamat <laithalamat4@gmail.com>
2022-06-11 19:54:46 +0300

    codechallenge done

---
+++ Data Structures/HashTables/HashTables/Node.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HashTables
+{
+    public class Node
+    {
+        public string Key { get; set; }
+        public object Value { get; set; }
+        public Node Next { get; set; }
+        public Node Previous { get; set; }
+    }
+}
---
+++ Data Structures/HashTables/HashTables/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HashTables
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            HashTable hashTable = new HashTable(50);
+            hashTable.Insert("ab", 5);
+            hashTable.Insert("bB", 6);
+            hashTable.Insert("aB", 7);
+            hashTable.Insert("Bc", 8);
+            hashTable.Insert("cA", 9);
+            hashTable.Insert("CC", 10);
+            hashTable.Insert("Bb", 66);
+
+
+            hashTable.GetKeys(hashTable);
+            Console.ReadLine();
+
+        }
+
+
+    }
+}
