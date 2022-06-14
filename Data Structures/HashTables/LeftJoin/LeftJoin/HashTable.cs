e6172d1792d0aca4aef059c8c68e5e1d5172d5cf
LaithAlamat <laithalamat4@gmail.com>
2022-06-14 18:38:08 +0300

    left join done, tests left

---
+++ Data Structures/HashTables/LeftJoin/LeftJoin/HashTable.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeftJoin
+{
+    public class HashTable
+    {
+        private Node[] universe;
+        private readonly int tableSize;
+
+        public HashTable(int maxTableSize)
+        {
+            tableSize = maxTableSize;
+            universe = new Node[tableSize];
+        }
+
+        public int HashFuncation(string key)
+        {
+            int index = 7;
+            int asciiVal = 0;
+            for (int i = 0; i < key.Length; i++)
+            {
+                asciiVal = (int)key[i] * i;
+                index = index * 31 + asciiVal;
+            }
+            return index % tableSize;
+        }
+        public void Insert(string key, object value)
+        {
+            int genIndex = HashFuncation(key);
+            Node node = universe[genIndex];
+
+            if (node == null)
+            {
+                universe[genIndex] = new Node() { Key = key, Value = value };
+                return;
+            }
+
+            if (node.Key == key)
+                throw new Exception("Can't use same key!");
+
+            while (node.Next != null)
+            {
+                node = node.Next;
+                if (node.Key == key)
+                    throw new Exception("Can't use same key!");
+            }
+
+            Node newNode = new Node() { Key = key, Value = value, Previous = node, Next = null };
+            node.Next = newNode;
+        }
+        public object GetValue(string key)
+        {
+            int genIndex = HashFuncation(key);
+            Node node = universe[genIndex];
+            while (node != null)
+            {
+                if (node.Key == key)
+                {
+                    return node.Value;
+                }
+                node = node.Next;
+            }
+
+            return null;
+        }
+
+        public List<string> GetKeys(HashTable input)
+        {
+            List<string> output = new List<string>();
+
+            for (int i = 0; i < input.universe.Length - 1; i++)
+            {
+                if (input.universe[i] != null)
+                {
+
+                    output.Add(input.universe[i].Key);
+                }
+            }
+
+            foreach (var item in output)
+            {
+                Console.WriteLine(item);
+            }
+
+            return output;
+        }
+
+        public bool Contains(string key)
+        {
+            int genIndex = HashFuncation(key);
+            Node node = universe[genIndex];
+            while (node != null)
+            {
+                if (node.Key == key)
+                {
+                    return true;
+                }
+                node = node.Next;
+            }
+
+            return false;
+        }
+
+        public void Set(string key, object value)
+        {
+            int index = HashFuncation(key);
+
+            if (universe[index] == null)
+            {
+                universe[index] = new Node() { Key = key, Value = value };
+            }
+            else
+            {
+                while (universe[index].Next != null)
+                {
+                    universe[index] = universe[index].Next;
+                    if (universe[index].Key == key)
+                        throw new Exception("Can't use same key!");
+                }
+
+                Node newNode = new Node() { Key = key, Value = value, Previous = universe[index], Next = null };
+                universe[index].Next = newNode;
+            }
+        }
+
+    }
+}
---
+++ Data Structures/HashTables/LeftJoin/LeftJoin/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LeftJoin
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            HashTable map1 = new HashTable(50);
+            map1.Insert("diligent", "employed");
+            map1.Insert("fond", "enamored");
+            map1.Insert("guide", "usher");
+            map1.Insert("outfit", "garb");
+            map1.Insert("wrath", "anger");
+            HashTable map2 = new HashTable(50);
+            map2.Insert("diligent", "idle");
+            map2.Insert("fond", "averse");
+            map2.Insert("guide", "follow");
+            map2.Insert("flow", "jam");
+            map2.Insert("wrath", "delight");
+            LeftJoin(map1, map2);
+            List<string> keys = map1.GetKeys(map1);
+            foreach (var item in keys)
+            {
+                Console.WriteLine(map1.GetValue(item));
+            }
+        }
+
+
+        public static HashTable LeftJoin(HashTable map1, HashTable map2)
+        {
+            foreach (var key in map2.GetKeys(map2))
+            {
+                int hashIndex = map2.HashFuncation(key);
+                if (!map1.Contains(key))
+                {
+                    Console.WriteLine("Key does not exist");
+                }
+                else
+                {
+                    object answer = map2.GetValue(key);
+                    map1.Set(key, answer);
+                }
+            }
+            return map1;
+        }
+    }
+}
