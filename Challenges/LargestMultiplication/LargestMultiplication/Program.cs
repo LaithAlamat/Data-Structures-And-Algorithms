b1a92449aec0b7790023cd9ea1a013ca0cacbf95
LaithAlamat <laithalamat4@gmail.com>
2022-07-12 22:51:38 +0300

    Challenge 44 done

---
+++ Challenges/LargestMultiplication/LargestMultiplication/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LargestMultiplication
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = new List<int>() { 7, 3, 5, 2 };
+            Console.WriteLine(Largest(numbers));
+        }
+
+        public static int Largest(List<int> nums)
+        {
+            
+            var sorted = new List<int>(nums.OrderBy(num => num));
+
+            int product = sorted[sorted.Count - 1] * sorted[sorted.Count - 2] * sorted[sorted.Count - 3];
+
+            return product;
+        }
+    }
+}
